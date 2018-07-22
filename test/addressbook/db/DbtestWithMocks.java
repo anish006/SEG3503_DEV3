5357718740e627937c8ee04ee9954b6a2043c7d2
gmonz36 <gmonzerol@live.fr>
2018-07-22 11:28:05 -0400

    finished db tests

--- test/addressbook/db/DbtestWithMocks.java
+++ test/addressbook/db/DbtestWithMocks.java
@@ -1,24 +1,14 @@
-/**
- * 
- */
 package addressbook.db;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.HashMap;
-
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.mockrunner.jdbc.CallableStatementResultSetHandler;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mockrunner.jdbc.JDBCTestModule;
 import com.mockrunner.jdbc.PreparedStatementResultSetHandler;
 import com.mockrunner.jdbc.StatementResultSetHandler;
@@ -26,35 +16,22 @@ import com.mockrunner.mock.jdbc.JDBCMockObjectFactory;
 import com.mockrunner.mock.jdbc.MockConnection;
 import com.mockrunner.mock.jdbc.MockPreparedStatement;
 import com.mockrunner.mock.jdbc.MockResultSet;
-import com.mockrunner.mock.jdbc.MockStatement;
-import java.util.List;
-
 
 import addressbook.core.Address;
 
+import java.sql.SQLException;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-import addressbook.core.Address;
 import addressbook.db.AddressDao;
 import addressbook.ui.ListEntry;
 
-import com.mockrunner.jdbc.BasicJDBCTestCaseAdapter;
-
 public class DbtestWithMocks {
 	
-	 private AddressDao aDao;
+	 private AddressDao addressDao;
 	 private Address address;
-	 private MockResultSet result; 
-	 PreparedStatementResultSetHandler resultHandler;
-	 MockStatement mStatement;
-	 MockConnection connection;
-	private JDBCMockObjectFactory mockFactory;
-	private JDBCTestModule jdbcModule;
+	 private MockResultSet result;
+	 private MockConnection connection;
+	 private JDBCMockObjectFactory mockFactory;
+	 private JDBCTestModule jdbcModule;
 	
 	 private int id = 0;
 	 private String lastName = "a_-zAZ";
@@ -69,27 +46,57 @@ public class DbtestWithMocks {
 	 private String postalCode = "a_-zAZ";
 	 private String country = "Canada";
 	 
+	    private static final String strGetAddress =
+	            "SELECT * FROM APP.ADDRESS " +
+	            "WHERE ID = ?";
+	    
+	    private static final String strSaveAddress =
+	            "INSERT INTO APP.ADDRESS " +
+	            "   (LASTNAME, FIRSTNAME, MIDDLENAME, PHONE, EMAIL, ADDRESS1, ADDRESS2, " +
+	            "    CITY, STATE, POSTALCODE, COUNTRY) " +
+	            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	    
+	    
+	    private static final String strGetListEntries =
+	            "SELECT ID, LASTNAME, FIRSTNAME, MIDDLENAME FROM APP.ADDRESS "  +
+	            "ORDER BY LASTNAME ASC";
+	    
+	    private static final String strUpdateAddress =
+	            "UPDATE APP.ADDRESS " +
+	            "SET LASTNAME = ?, " +
+	            "    FIRSTNAME = ?, " +
+	            "    MIDDLENAME = ?, " +
+	            "    PHONE = ?, " +
+	            "    EMAIL = ?, " +
+	            "    ADDRESS1 = ?, " +
+	            "    ADDRESS2 = ?, " +
+	            "    CITY = ?, " +
+	            "    STATE = ?, " +
+	            "    POSTALCODE = ?, " +
+	            "    COUNTRY = ? " +
+	            "WHERE ID = ?";
+	    
+	    private static final String strDeleteAddress =
+	            "DELETE FROM APP.ADDRESS " +
+	            "WHERE ID = ?";
+	 
 	 @Before
 	 public void setUp() throws Exception {
 		 prepareResultSet();
 		 address = new Address(lastName, firstName, middleName, phone, email, address1, address2, city, state, 
 				 postalCode, country, id);
-		 mStatement=new MockStatement(connection);
-		 aDao = new AddressDao( );
-		 aDao.connect();
-		 //aDao.setDbaccess(connection);
+		 addressDao = new AddressDao( );
+		 addressDao.connect();
 	 }
 	 
 	 private void prepareResultSet() {
 		 mockFactory = new JDBCMockObjectFactory();
 		 jdbcModule = new JDBCTestModule(mockFactory);
 		 connection = mockFactory.getMockConnection();
-		 PreparedStatementResultSetHandler statementHandler =
-		 connection.getPreparedStatementResultSetHandler();
-		 MockPreparedStatement mockstatement =
-		 new MockPreparedStatement(connection, "SELECT * from CUSTOMER");
+		 PreparedStatementResultSetHandler statementHandler = connection.getPreparedStatementResultSetHandler();
+		 MockPreparedStatement mockstatement = new MockPreparedStatement(connection, "SELECT * from CUSTOMER");
 		 statementHandler.addPreparedStatement(mockstatement);
-		 /*MockResultSet*/ result = statementHandler.createResultSet();
+		 result = statementHandler.createResultSet();
 		 result.addColumn("ID");
 		 result.addColumn("LASTNAME");
 		 result.addColumn("FIRSTNAME");
@@ -126,43 +133,9 @@ public class DbtestWithMocks {
 	 
 	 @After
 	 public void tearDown() throws Exception { 
-		 //super.tearDown();
+		 addressDao.disconnect();
 	 }
 	 
-	 private static final String strGetAddress =
-			 "SELECT * FROM APP.ADDRESS " +
-		     "WHERE ID = ?";
-	 
-	 private static final String strSaveAddress =
-			 "INSERT INTO APP.ADDRESS " +
-		     "   (LASTNAME, FIRSTNAME, MIDDLENAME, PHONE, EMAIL, ADDRESS1, ADDRESS2, " +
-		     "    CITY, STATE, POSTALCODE, COUNTRY) " +
-		     "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-	
-	 private static final String strGetListEntries =
-			 "SELECT ID, LASTNAME, FIRSTNAME, MIDDLENAME FROM APP.ADDRESS "  +
-		     "ORDER BY LASTNAME ASC";
-	 
-	 private static final String strUpdateAddress =
-			 "UPDATE APP.ADDRESS " +
-		     "SET LASTNAME = ?, " +
-		     "    FIRSTNAME = ?, " +
-		     "    MIDDLENAME = ?, " +
-		     "    PHONE = ?, " +
-		     "    EMAIL = ?, " +
-		     "    ADDRESS1 = ?, " +
-		     "    ADDRESS2 = ?, " +
-		     "    CITY = ?, " +
-		     "    STATE = ?, " +
-		     "    POSTALCODE = ?, " +
-		     "    COUNTRY = ? " +
-		     "WHERE ID = ?";
-		    
-	 private static final String strDeleteAddress =
-			 "DELETE FROM APP.ADDRESS " +
-		     "WHERE ID = ?";
-	 
-	
 	 @Test
 	 public void testSaveRecord() throws Exception {
 		 List<Object> rowItems = new ArrayList<Object>();
@@ -179,10 +152,8 @@ public class DbtestWithMocks {
 		 rowItems.add(postalCode);
 		 rowItems.add(country);
 		 result.addRow(rowItems);
-		 //resultHandler.prepareGlobalGeneratedKeys(result);
 		 int expected = -1;
-		 int actual = aDao.saveRecord(address);
-		 System.out.println(actual);
+		 int actual = addressDao.saveRecord(address);
 		 assertEquals(expected, actual);
 		 jdbcModule.verifyPreparedStatementPresent(strSaveAddress);
 		 jdbcModule.verifySQLStatementExecuted(strSaveAddress);
@@ -201,7 +172,6 @@ public class DbtestWithMocks {
 	 
 	 @Test
 	 public void testEditRecord() throws SQLException {
-		 int id = 0;
 		 List<Object> rowItems = new ArrayList<Object>();
 		 rowItems.add(0);
 		 rowItems.add(lastName);
@@ -218,14 +188,11 @@ public class DbtestWithMocks {
 		 result.addRow(rowItems);
 		 String changedLastName = "John";
 		 String changedFirstName = "Wayne";
-		 //result.getRow(0).set(1, changedLastName);
-		 //result.getRow(0).set(2, changedFirstName);
-		 //resultHandler.prepareGlobalGeneratedKeys(result);
-		 aDao.saveRecord(address);
+		 addressDao.saveRecord(address);
 		 address.setLastName("John");
 		 address.setFirstName("Wayne");
 		 boolean expected = true;
-		 boolean actual = aDao.editRecord(address);
+		 boolean actual = addressDao.editRecord(address);
 		 assertEquals(expected, actual);
 		 jdbcModule.verifyPreparedStatementPresent(strUpdateAddress);
 		 jdbcModule.verifySQLStatementExecuted(strUpdateAddress);
@@ -244,26 +211,28 @@ public class DbtestWithMocks {
 	 
 	 @Test
 	 public void testDeleteRecord() throws SQLException {
-		 int id = 1;
-		 result.addRow(new Object[] { id, 
-				 lastName, 
-				 firstName, 
-				 middleName, 
-				 phone, 
-				 email, 
-				 address1, 
-				 address2, 
-				 city, 
-				 state, 
-				 postalCode, 
-				 country });
-		 aDao.saveRecord(address);
+		 int idToDelete = 5;
+		 List<Object> rowItems = new ArrayList<Object>();
+		 rowItems.add(idToDelete);
+		 rowItems.add(lastName);
+		 rowItems.add(firstName);
+		 rowItems.add(middleName);
+		 rowItems.add(phone);
+		 rowItems.add(email);
+		 rowItems.add(address1);
+		 rowItems.add(address2);
+		 rowItems.add(city);
+		 rowItems.add(state);
+		 rowItems.add(postalCode);
+		 rowItems.add(country);
+		 result.addRow(rowItems);
+		 addressDao.saveRecord(address);
 		 boolean expected = true;
-		 boolean actual = aDao.deleteRecord(id);
+		 boolean actual = addressDao.deleteRecord(idToDelete);
 		 assertEquals(expected, actual);
 		 jdbcModule.verifyPreparedStatementPresent(strDeleteAddress);
 		 jdbcModule.verifySQLStatementExecuted(strDeleteAddress);
-		 jdbcModule.verifySQLStatementParameter(strDeleteAddress, 0, 1, id);
+		 jdbcModule.verifySQLStatementParameter(strDeleteAddress, 0, 1, idToDelete);
 		 jdbcModule.verifySQLStatementParameter(strDeleteAddress, 0, 2, null);
 		 jdbcModule.verifySQLStatementParameter(strDeleteAddress, 0, 3, null);
 		 jdbcModule.verifySQLStatementParameter(strDeleteAddress, 0, 4, null);
@@ -291,10 +260,8 @@ public class DbtestWithMocks {
 		 result.addColumn("POSTALCODE", new Object[]{ postalCode });
 		 result.addColumn("COUNTRY", new Object[]{ country });
 		 result.addColumn("ID", new Object[]{ id });
-		 //resultHandler.prepareResultSet(strGetAddress, result);
-		 //resultHandler.prepareGlobalGeneratedKeys(result);
 		 Address expected = address;
-		 Address actual = aDao.getAddress(id);		
+		 Address actual = addressDao.getAddress(id);		
 		 assertTrue(expected.equals(actual));
 		 jdbcModule.verifyPreparedStatementPresent(strGetAddress);
 		 jdbcModule.verifySQLStatementExecuted(strGetAddress);
@@ -303,35 +270,45 @@ public class DbtestWithMocks {
 	 
 	 @Test
 	 public void testGetListEntries() {
-		 
-		 StatementResultSetHandler statementHandler =
-				 connection.getStatementResultSetHandler();
-				 MockResultSet result = statementHandler.createResultSet();
+		StatementResultSetHandler statementHandler = connection.getStatementResultSetHandler();
+		MockResultSet result = statementHandler.createResultSet(); 
+		result.addColumn("ID", new Object[]{ id });
+		result.addColumn("LASTNAME", new Object[]{ lastName });
+		result.addColumn("FIRSTNAME", new Object[]{ firstName });
+		result.addColumn("MIDDLENAME", new Object[]{ middleName });
+		statementHandler.prepareResultSet(strGetListEntries,result);
 				
-				 
-				 result.addColumn("ID", new Object[]{ id });
-				 result.addColumn("LASTNAME", new Object[]{ lastName });
-				 result.addColumn("FIRSTNAME", new Object[]{ firstName });
-				 result.addColumn("MIDDLENAME", new Object[]{ middleName });
-		
-		 statementHandler.prepareResultSet(strGetListEntries,result);
-				
-		
-		 List<ListEntry> listEntries = aDao.getListEntries();
-		 ListEntry listEntry = listEntries.get(0);
+		List<ListEntry> listEntries = addressDao.getListEntries();
+		ListEntry listEntry = listEntries.get(0);
 			
-		 int entryCount = listEntries.size();
-		 assertEquals(1, entryCount);
+		int entryCount = listEntries.size();
+		assertEquals(1, entryCount);
 			
-		 assertEquals(id, listEntry.getId());	
-		 assertEquals(lastName, listEntry.getLastName());
-		 assertEquals(firstName, listEntry.getFirstName());
-		 assertEquals(middleName, listEntry.getMiddleName());
+		assertEquals(id, listEntry.getId());	
+		assertEquals(lastName, listEntry.getLastName());
+		assertEquals(firstName, listEntry.getFirstName());
+		assertEquals(middleName, listEntry.getMiddleName());
 		 
-		 jdbcModule.verifySQLStatementExecuted(strGetListEntries);
+		jdbcModule.verifySQLStatementExecuted(strGetListEntries);
 	 }
 	 
-	
+	 @Test
+	 public void testDeleteRecordWithAddress() {
+		 addressDao.deleteRecord(address);
+		 jdbcModule.verifyPreparedStatementPresent(strDeleteAddress);
+		 jdbcModule.verifySQLStatementExecuted(strDeleteAddress);
+		 jdbcModule.verifySQLStatementParameter(strDeleteAddress, 0, 1, id);
+		 jdbcModule.verifySQLStatementParameter(strDeleteAddress, 0, 2, null);
+		 jdbcModule.verifySQLStatementParameter(strDeleteAddress, 0, 3, null);
+		 jdbcModule.verifySQLStatementParameter(strDeleteAddress, 0, 4, null);
+		 jdbcModule.verifySQLStatementParameter(strDeleteAddress, 0, 5, null);
+		 jdbcModule.verifySQLStatementParameter(strDeleteAddress, 0, 6, null);
+		 jdbcModule.verifySQLStatementParameter(strDeleteAddress, 0, 7, null);
+		 jdbcModule.verifySQLStatementParameter(strDeleteAddress, 0, 8, null);
+		 jdbcModule.verifySQLStatementParameter(strDeleteAddress, 0, 9, null);
+		 jdbcModule.verifySQLStatementParameter(strDeleteAddress, 0, 10, null);
+		 jdbcModule.verifySQLStatementParameter(strDeleteAddress, 0, 11, null);
+	 }
 	
 	
 
